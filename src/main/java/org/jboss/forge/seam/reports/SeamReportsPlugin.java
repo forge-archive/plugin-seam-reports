2a51f38dd2265b7adc78caf43f99f376c0c8aa2d
George Gastaldi <gegastaldi@gmail.com>
2011-05-31 16:11:52 -0300

    SEAMREPORTS-13

--- src/main/java/org/jboss/forge/seam/reports/SeamReportsPlugin.java
+++ src/main/java/org/jboss/forge/seam/reports/SeamReportsPlugin.java
@@ -1,5 +1,53 @@
 package org.jboss.forge.seam.reports;
 
-public class SeamReportsPlugin {
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.codehaus.plexus.util.StringUtils;
+import org.jboss.forge.project.Project;
+import org.jboss.forge.project.dependencies.Dependency;
+import org.jboss.forge.project.dependencies.DependencyBuilder;
+import org.jboss.forge.project.facets.DependencyFacet;
+import org.jboss.forge.shell.ShellPrompt;
+import org.jboss.forge.shell.plugins.Alias;
+import org.jboss.forge.shell.plugins.Command;
+import org.jboss.forge.shell.plugins.Option;
+import org.jboss.forge.shell.plugins.Plugin;
+import org.jboss.forge.shell.plugins.RequiresFacet;
+
+@Alias("seam-reports")
+@RequiresFacet({ DependencyFacet.class })
+public class SeamReportsPlugin implements Plugin {
+
+    @Inject
+    Project project;
+    @Inject
+    ShellPrompt prompt;
+
+    @Command(value = "setup")
+    public void setup(@Option(name = "provider", completer = ProvidersTokenCompleter.class) String provider) {
+        DependencyFacet dependencyFacet = project.getFacet(DependencyFacet.class);
+        DependencyBuilder seamReportsDependency = DependencyBuilder.create().setGroupId("org.jboss.seam.reports")
+                .setArtifactId("seam-reports-api");
+
+        if (!dependencyFacet.hasDependency(seamReportsDependency)) {
+            if (!dependencyFacet.hasRepository(DependencyFacet.KnownRepository.JBOSS_NEXUS)) {
+                dependencyFacet.addRepository(DependencyFacet.KnownRepository.JBOSS_NEXUS);
+            }
+
+            List<Dependency> versions = dependencyFacet.resolveAvailableVersions(seamReportsDependency);
+
+            Dependency choosenVersion = prompt.promptChoiceTyped("Which version of Seam Reports do you want to install?",
+                    versions, versions.get(versions.size() - 1));
+            dependencyFacet.setProperty("seam.reports.version", choosenVersion.getVersion());
+
+            dependencyFacet.addDependency(seamReportsDependency.setVersion("${seam.reports.version}"));
+            if (StringUtils.isNotBlank(provider)) {
+                dependencyFacet.addDependency(DependencyBuilder.create().setGroupId("org.jboss.seam.reports")
+                        .setArtifactId("seam-reports-" + provider.toLowerCase()).setVersion("${seam.reports.version}"));
+            }
+        }
+    }
 
 }
